b14b344a65b394b294a2f1edac3e7a04d429f9b9
oluchia <o.anyabuike1@nuigalway.ie>
2017-03-29 12:12:07 +0100

    Create router.js
    .js file for various routes to templates. info4 router is missing on purpose.

---
+++ client/lib/router.js
@@ -0,0 +1,22 @@
+Router.route('/', {
+    template: 'home'
+});
+
+Router.route('/aboutUs');
+Router.route('/findUs');
+Router.route('/contactUs');
+Router.route('/profile');
+Router.route('/tvshows');
+Router.route('/movies');
+Router.route('/info');
+Router.route('/info1');
+Router.route('/info2');
+Router.route('/info3');
+Router.route('/info5');
+Router.route('/info6');
+Router.route('/info7');
+Router.route('/info8');
+Router.route('/info9');
+Router.route('/info10');
+Router.route('/info11');
+Router.route('/info12');
